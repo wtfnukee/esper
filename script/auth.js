0c6c4f26bd7df7582476f2428d85618e638e8c8a
wtfnukee <hehenukee@gmail.com>
2020-03-12 16:25:13 +0300

    Auth work

--- script/auth.js
+++ script/auth.js
@@ -1,3 +1,19 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyADl-M9rosKLQhgYqPX73OChXnkQX3Z_80",
+    authDomain: "esperproj.firebaseapp.com",
+    databaseURL: "https://esperproj.firebaseio.com",
+    projectId: "esperproj",
+    storageBucket: "esperproj.appspot.com",
+    messagingSenderId: "317958487855",
+    appId: "1:317958487855:web:3d018d8d2fca24a3ab6bda",
+    measurementId: "G-49116J0KB3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
 function handleSignUp() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
@@ -26,3 +42,17 @@ function handleSignUp() {
     });
     // [END createwithemail]
 }
+
+const txtEmail = document.getElementById("txtEmail");
+const txtPassword = document.getElementById("txtPassword");
+const btnSignUp = document.getElementById("btnSignUp");
+const btnLogin = document.getElementById("btnLogin");
+
+btnLogin.addEventListener('click', e => {
+    const email = txtEmail.value;
+    const pass = txtEmail.value;
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+
+});
