3356555452425ddce3993cea6614f6ebed8f23da
wtfnukee <wtfnukee@gmail.com>
2020-03-30 14:31:23 +0300

    Update db.js

--- script/db.js
+++ script/db.js
@@ -6,39 +6,26 @@ const btnPushData = document.getElementById("pushData");
 const inputUsername = document.getElementById("inputUsername");
 const inputText = document.getElementById("inputText");
 
-const user = firebase.auth().currentUser;
 
-firebase.auth().onAuthStateChanged(function(user) {
-if (user) {
-    // User is signed in.
-     const userId = user.uid;
-
-    function pushData() {
-        firebase.firestore().collection("/UserSandbox").add({
-            username: inputUsername.value,
-            text: inputText.value,
-            uid: userId,
-            timestamp: firebase.firestore().FieldValue.serverTimestamp()
+function pushData() {
+    firebase.firestore().collection("/UserSandbox").add({
+        username: inputUsername.value,
+        text: inputText.value,
+        timestamp: firebase.firestore().FieldValue.serverTimestamp()
+    })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
         })
-            .then(function(docRef) {
-                console.log("Document written with ID: ", docRef.id);
-            })
-            .catch(function(error) {
-                console.error("Error adding document: ", error);
-            });
-    }
-    
-    //Read data
-    firebase.firestore().collection("/UserSandbox").doc("SF")
-        .onSnapshot(function(doc) {
-            alert("ЧТЕНИЕ РАБОТАЕТ");
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
         });
-
-} else {
-    // No user is signed in.
-    alert('No user is signed in.');
 }
-});
+//Read data
+firebase.firestore().collection("/UserSandbox").doc("SF")
+    .onSnapshot(function(doc) {
+        alert("ЧТЕНИЕ РАБОТАЕТ");
+    });
+
 
 //Add button listeners
 btnPushData.addEventListener('click', pushData, false);
